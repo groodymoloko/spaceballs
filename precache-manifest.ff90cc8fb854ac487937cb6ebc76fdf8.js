4310b63d7a5f2ffb5c9b8cf3f7726e59463a3948
groodymoloko <44014448+groodymoloko@users.noreply.github.com>
2019-03-29 20:49:00 -0700

    Updates

---
+++ precache-manifest.ff90cc8fb854ac487937cb6ebc76fdf8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5461f41a1280f3087b19671959797893",
+    "url": "/groodymoloko/spaceballs.git/static/media/starjedi-webfont.5461f41a.ttf"
+  },
+  {
+    "revision": "90c8b97781d5c0e23df284d785dcbd4f",
+    "url": "/groodymoloko/spaceballs.git/static/media/rv.90c8b977.jpg"
+  },
+  {
+    "revision": "65cf4d05ca1253625bedd8b556e47838",
+    "url": "/groodymoloko/spaceballs.git/static/media/background.65cf4d05.png"
+  },
+  {
+    "revision": "bf7bcb8d8a645cecc481",
+    "url": "/groodymoloko/spaceballs.git/static/js/runtime~main.0205c696.js"
+  },
+  {
+    "revision": "8618d04b69825e53ea60",
+    "url": "/groodymoloko/spaceballs.git/static/js/main.5258fdf6.chunk.js"
+  },
+  {
+    "revision": "d253f265b44bfb1849d5",
+    "url": "/groodymoloko/spaceballs.git/static/js/2.eb4a84e2.chunk.js"
+  },
+  {
+    "revision": "8618d04b69825e53ea60",
+    "url": "/groodymoloko/spaceballs.git/static/css/main.45faeca5.chunk.css"
+  },
+  {
+    "revision": "43cc13d11b92c2adb57165d96a6f3ca4",
+    "url": "/groodymoloko/spaceballs.git/index.html"
+  }
+];
